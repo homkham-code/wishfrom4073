bf8c433cc6083519c27f9aca316c494f89ba67f2
homkham-code <88870580+homkham-code@users.noreply.github.com>
2021-08-13 20:29:28 +0630

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,25 +9,24 @@ const wish = document.getElementById('wish');
 const text="I wish you a fulfilling life and many more years of joy, happiness and success as you spread your knowledge. Happy Birthday to an  awesome teacher";
 
 let idx = 1;
-let speed = 100;
+let speed = 90;
 
 function autotext() {
     // console.log('hay');
 
+    textel.style.opacity = 1;
     textel.innerText = text.slice(0,idx);
     textel.style.direction = "rtl";
     idx++;
-
+    
     setTimeout(autotext,speed);
 }
 
 //Event Listener
 wish.addEventListener('click',()=>{
     //console.log("hay");
+    textel.style.opacity = 0;
+    idx = 1;
     autotext();
     audio.play();
 });
-
-
-
-
